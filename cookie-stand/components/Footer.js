92960ddfa24812bdf0bc93c580162be93ca43551
Arthur <chillout885@gmail.com>
2022-04-07 21:50:54 -0700

    complete

--- cookie-stand/components/Footer.js
+++ cookie-stand/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer(props) {
   return (
-    <footer className="p-3 bg-green-500 nt-10">
+    <footer className="p-3 bg-blue-500 nt-10">
       {/* <h1>{props.resources ? props.resources.length : 0} Locations World Wide</h1> */}
       <p>&copy; 2022</p>
     </footer>
